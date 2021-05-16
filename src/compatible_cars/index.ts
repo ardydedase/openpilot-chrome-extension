7bc26451511404caec527ee307be1a860cdc8221
ardydedase <ardy.dedase@gmail.com>
2021-05-16 10:26:10 -0600

    Working community support cars

--- src/compatible_cars/index.ts
+++ src/compatible_cars/index.ts
@@ -1,8 +1,4 @@
-import { Compatibility } from '../interface';
 import { supportedCars } from './supported';
 import { communityMaintainedCars } from './community';
-// import { support } from 'jquery';
 
-// console.log('compatible cars: ', [...supportedCars, ...communityMaintainedCars]);
-
-export const compatibleCars = [...supportedCars];
+export const compatibleCars = [...supportedCars, ...communityMaintainedCars];
