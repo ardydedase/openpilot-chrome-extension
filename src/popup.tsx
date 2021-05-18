c36c437fa2d04e7218c7be8bdc4c0b5fe8e2c52d
ardydedase <ardy.dedase@gmail.com>
2021-05-17 20:18:58 -0600

    Icons and popup updates

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,45 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 
 export const Popup = () => {
-  const [count, setCount] = useState(0);
-  const [currentURL, setCurrentURL] = useState<string>();
-
-  useEffect(() => {
-    chrome.browserAction.setBadgeText({ text: count.toString() });
-  }, [count]);
-
-  useEffect(() => {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      setCurrentURL(tabs[0].url);
-    });
-  }, []);
-
-  const changeBackground = () => {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      const tab = tabs[0];
-      if (tab.id) {
-        chrome.tabs.sendMessage(
-          tab.id,
-          {
-            color: "#555555",
-          },
-          (msg) => {
-            console.log("result message:", msg);
-          }
-        );
-      }
-    });
-  };
-
   return (
     <>
       <div style={{ minWidth: "300px" }}>
         <div>Openpilot Compatible Cars (Unofficial)</div>
-        <div>Built by <a href="https://github.com/ardydedase/">@ardydedase</a></div>
+        <div>Built by <a href="https://github.com/ardydedase/" target="_blank">@ardydedase</a></div>
         <div>Curently works on autotrader.ca</div>
-        <div>MIT License</div>
-        <div>Contribute to support more websites</div>
+        <div><a href="https://github.com/ardydedase/openpilot-chrome-plugin" target="_blank">Contribute on GitHub</a> to support more websites</div>
+        <div><a href="https://github.com/ardydedase/openpilot-chrome-plugin/blob/master/LICENSE" target="_blank">MIT License</a></div>        
       </div>
     </>
   );
